ad9aec78a80f3487d2e55336d74d2e04aadfbd08
affandeZone <affandes@gmail.com>
2016-02-20 14:52:46 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Sistem-TA.
+ * Copyright (c) 2016 affandeZone.
+ * e : affandes@gmail.com
+ * index.php 2/20/16 2:47 PM
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hi - Sistem TA</title>
+</head>
+<body>
+<h1>Ooppss...</h1>
+<p>We are working to finish this nice apps. Please come later.</p>
+</body>
+</html>
